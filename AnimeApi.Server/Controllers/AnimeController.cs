5f3edef41a7a18f794c4041dd3c0261dc58cd516
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2025-04-25 00:17:25 +0200

    Anime controlller get by episodes and release year endpoints

--- AnimeApi.Server/Controllers/AnimeController.cs
+++ AnimeApi.Server/Controllers/AnimeController.cs
@@ -14,9 +14,10 @@ public class AnimeController : ControllerBase
         _helper = helper;
     }
     [HttpGet]
+    [Route("{id:int:min(1)}")]
     [ProducesResponseType(200)]
     [ProducesResponseType(404)]
-    public async Task<IActionResult> GetByIdAsync([FromQuery] int id)
+    public async Task<IActionResult> GetByIdAsync([FromRoute] int id)
     {
         var anime = await _helper.GetByIdAsync(id);
         if (anime is null) return NotFound();
@@ -27,9 +28,33 @@ public class AnimeController : ControllerBase
     [Route("search")]
     [ProducesResponseType(200)]
     [ProducesResponseType(404)]
-    public async Task<IActionResult> GetByIdAsync([FromQuery] AnimeSearchParameters parameters)
+    public async Task<IActionResult> GetByParametersAsync([FromQuery] AnimeSearchParameters parameters)
     {
         var anime = await _helper.SearchAsync(parameters);
+        return Ok(anime);
+    }
+
+    [HttpGet]
+    [Route("year/{year:int:min(1900)}")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> GetByReleaseYearAsync([FromRoute] int year)
+    {
+        var anime = await _helper.GetByReleaseYearAsync(year);
+        if (!anime.Any()) return NotFound();
+        
+        return Ok(anime);
+    }
+
+    [HttpGet]
+    [Route("episodes/{episodes:int:min(1)}")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> GetByEpisodes([FromRoute] int episodes)
+    {
+        var anime = await _helper.GetByEpisodesAsync(episodes);
+        if(!anime.Any()) return NotFound();
+        
         return Ok(anime);
     }
 }
