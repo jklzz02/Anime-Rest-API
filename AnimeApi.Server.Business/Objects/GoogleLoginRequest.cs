b121e108304db9bca80d39b1ec5b14580aaac4b1
jklzz02 <jacopo.lazzaro@gmail.com>
2025-07-05 21:01:40 +0200

    Login request
    added json property attribute

--- AnimeApi.Server.Business/Objects/GoogleLoginRequest.cs
+++ AnimeApi.Server.Business/Objects/GoogleLoginRequest.cs
@@ -1,6 +1,9 @@
+using Newtonsoft.Json;
+
 namespace AnimeApi.Server.Business.Objects;
 
 public class GoogleLoginRequest
 {
+    [JsonProperty("id_token")]
     public string IdToken { get; set; } = string.Empty;
 }
