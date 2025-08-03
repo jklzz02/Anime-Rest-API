ec22004b26c5275bb3d342a85a751ab5222e404e
jklzz02 <jacopo.lazzaro@gmail.com>
2025-08-03 22:47:17 +0200

    User favourite endpoints

--- AnimeApi.Server/Controllers/UserController.cs
+++ AnimeApi.Server/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 using System.Security.Claims;
 using AnimeApi.Server.Core.Abstractions.Business.Services;
+using AnimeApi.Server.Core.Objects.Dto;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,10 +11,12 @@ namespace AnimeApi.Server.Controllers;
 public class UserController : ControllerBase
 {
     private readonly IUserService _userService;
+    private readonly IFavouritesHelper _favouritesHelper;
     
-    public UserController(IUserService userService)
+    public UserController(IUserService userService, IFavouritesHelper favouritesHelper)
     {
         _userService = userService;
+        _favouritesHelper = favouritesHelper;
     }
 
     [HttpGet]
@@ -31,6 +34,57 @@ public class UserController : ControllerBase
         }
         
         return Ok(user);
+    }
+
+    [HttpGet]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> GetCurrentUserFavourites()
+    {
+        var email = User.FindFirst(ClaimTypes.Email);
+        var user = await _userService.GetByEmailAsync(email?.Value ?? string.Empty);
+
+        if (user is null)
+        {
+            return Unauthorized();
+        }
+
+        var favourites = await _favouritesHelper
+            .GetFavouritesAsync(user.Id);
+        
+        return Ok(favourites);
+    }
+
+    [HttpPost]
+    [Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> AddFavouriteAsync([FromBody] int animeId)
+    {
+        var email = User.FindFirst(ClaimTypes.Email);
+        var user = await _userService.GetByEmailAsync(email?.Value ?? string.Empty);
+
+        if (user is null)
+        {
+            return Unauthorized();
+        }
+
+        var favourite = new FavouriteDto
+        {
+            UserId = user.Id,
+            AnimeId = animeId
+        };
+        
+        var result = await _favouritesHelper.AddFavouriteAsync(favourite);
+        
+        if (!result)
+        {
+            return BadRequest(_favouritesHelper.ErrorMessages);
+        }
+        
+        return Ok(favourite);
     }
 
     [HttpDelete]
@@ -53,6 +107,38 @@ public class UserController : ControllerBase
         if (!result) 
         {
             Unauthorized();
+        }
+
+        return NoContent();
+    }
+    
+    [HttpDelete]
+    [Authorize]
+    [Route("{id:int:min(1)}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DestroyFavouriteAsync([FromRoute] int id)
+    {
+        var email = User.FindFirst(ClaimTypes.Email);
+        var user = await _userService.GetByEmailAsync(email?.Value ?? string.Empty);
+        
+        if (user is null)
+        {
+            return Unauthorized();
+        }
+
+        var favourite = new FavouriteDto
+        {
+            UserId = user.Id,
+            AnimeId = id
+        };
+        
+        var result = await _favouritesHelper.RemoveFavouriteAsync(favourite);
+        
+        if (!result)
+        {
+            return NotFound();
         }
 
         return NoContent();
