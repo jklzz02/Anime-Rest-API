d2d52a5115c58160a021ddb7ba04da66decf2b0e
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2025-10-17 22:10:03 +0200

    base repository update

--- AnimeApi.Server.Core/Abstractions/DataAccess/Services/IRepository.cs
+++ AnimeApi.Server.Core/Abstractions/DataAccess/Services/IRepository.cs
@@ -13,58 +13,77 @@ public interface IRepository<TEntity, TDto>
     where TDto : class
 {
     /// <summary>
-    /// Asynchronously retrieves all entities of the specified type.
+    /// 
     /// </summary>
-    /// <returns>
-    /// Returns a collection of entities of type <typeparamref name="TDto"/>.
-    /// </returns>
+    /// <returns></returns>
     Task<IEnumerable<TDto>> GetAllAsync();
 
     /// <summary>
-    /// Asynchronously retrieves the first entity that matches the given specification.
+    /// 
     /// </summary>
-    /// <param name="specification">The specification of type <typeparamref name="TEntity"/></param>
-    /// <returns>Returns the retrieved entity.</returns>
+    /// <param name="specification"></param>
+    /// <returns></returns>
     Task<TDto?> FindFirstOrDefaultAsync(ISpecification<TEntity> specification);
-
+    
     /// <summary>
-    /// Asynchronously retrieves all entities through the specified specification.
+    /// 
     /// </summary>
-    /// <param name="specification">The specification of type <typeparamref name="TEntity"/></param>
-    /// <returns>Returns a collection of entities based on the specification.</returns>
+    /// <param name="specification"></param>
+    /// <returns></returns>
     Task<IEnumerable<TDto>> FindAsync(ISpecification<TEntity> specification);
 
     /// <summary>
-    /// Asynchronously retrieves the count of entities that match the given specification.
+    /// 
     /// </summary>
-    /// <param name="specification">The specification.</param>
-    /// <returns>Returns the count of the entities that match the specification.</returns>
+    /// <returns></returns>
+    Task<int> CountAsync();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="specification"></param>
+    /// <returns></returns>
     Task<int> CountAsync(ISpecification<TEntity> specification);
 
     /// <summary>
-    /// Asynchronously adds a new entity of the specified type to the repository.
+    /// 
     /// </summary>
-    /// <param name="entity">The entity to be added.</param>
-    /// <returns>
-    /// Returns the added entity of type <typeparamref name="TEntity"/> if the operation succeeds; otherwise, null.
-    /// </returns>
+    /// <param name="entity"></param>
+    /// <returns></returns>
     Task<Result<TDto>> AddAsync(TEntity entity);
 
     /// <summary>
-    /// Asynchronously updates an existing entity of the specified type with new data.
+    /// 
     /// </summary>
-    /// <param name="entity">The entity containing updated data.</param>
-    /// <returns>
-    /// Returns the updated entity of type <typeparamref name="TEntity"/> if the operation is successful; otherwise, null.
-    /// </returns>
+    /// <param name="entity"></param>
+    /// <returns></returns>
+    Task<Result<IEnumerable<TDto>>> AddRangeAsync(IEnumerable<TEntity> entity);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <returns></returns>
     Task<Result<TDto>> UpdateAsync(TEntity entity);
 
     /// <summary>
-    /// Asynchronously deletes an entity of the specified type by its unique identifier.
+    /// 
     /// </summary>
-    /// <param name="id">The unique identifier of the entity to delete.</param>
-    /// <returns>
-    /// Returns true if the entity was successfully deleted; otherwise, false.
-    /// </returns>
-    Task<bool> DeleteAsync(int id);
+    /// <param name="specification"></param>
+    /// <returns></returns>
+    Task<bool> DeleteAsync(ISpecification<TEntity> specification);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="specification"></param>
+    /// <returns></returns>
+    Task<bool> DeleteRangeAsync(ISpecification<TEntity> specification);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="specification"></param>
+    /// <returns></returns>
+    Task<bool> ExistsAsync(ISpecification<TEntity> specification);
 }
--- AnimeApi.Server.DataAccess/Repositories/BaseRepository.cs
+++ AnimeApi.Server.DataAccess/Repositories/BaseRepository.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AnimeApi.Server.DataAccess.Repositories
 {
-    public abstract class BaseRepository<TEntity, TDto> : IRepository<TEntity, TDto>
+    public class BaseRepository<TEntity, TDto> : IRepository<TEntity, TDto>
         where TEntity : class
         where TDto : class
     {
@@ -31,53 +31,127 @@ namespace AnimeApi.Server.DataAccess.Repositories
 
         public async Task<IEnumerable<TDto>> FindAsync(ISpecification<TEntity> specification)
         {
-            var result = await
+            List<TEntity> result = await
                 specification
                     .Apply(_context.Set<TEntity>())
                     .ToListAsync();
 
-            return _mapper.MapTo(result);
+            return _mapper.MapToDto(result);
         }
 
         public async Task<TDto?> FindFirstOrDefaultAsync(ISpecification<TEntity> specification)
         {
-            var result = await
+            TEntity? result = await
                 specification
                     .Apply(_context.Set<TEntity>())
                     .FirstOrDefaultAsync();
 
             return result is null
                 ? null
-                : _mapper.MapTo(result);
+                : _mapper.MapToDto(result);
         }
         
 
         public async Task<IEnumerable<TDto>> GetAllAsync()
         {
-            var result = await 
+            List<TEntity> result = await
                 _context.Set<TEntity>()
                     .ToListAsync();
 
-            return _mapper.MapTo(result);
+            return _mapper.MapToDto(result);
         }
 
-        public abstract Task<Result<TDto>> AddAsync(TEntity entity);
+        public async Task<Result<TDto>> AddAsync(TEntity entity)
+        {
+           var createdEntity = await 
+                _context.Set<TEntity>()
+                    .AddAsync(entity);
 
-        public abstract Task<Result<TDto>> UpdateAsync(TEntity entity);
+            bool saveResult = await _context.SaveChangesAsync() > 0;
 
-        public abstract Task<bool> DeleteAsync(int id);
+            return saveResult
+                ? Result<TDto>.Success(_mapper.MapToDto(createdEntity.Entity))
+                : Result<TDto>.InternalFailure("Failed to add entity.", "An error occurred while saving the entity to the database.");
+        }
+
+        public async Task<Result<IEnumerable<TDto>>> AddRangeAsync(IEnumerable<TEntity> entity)
+        {
+            await _context.Set<TEntity>().AddRangeAsync(entity);
+            bool saveResult = await _context.SaveChangesAsync() > 0;
+
+            return saveResult
+                ? Result<IEnumerable<TDto>>.Success(_mapper.MapToDto(entity))
+                : Result<IEnumerable<TDto>>.InternalFailure("Failed to add entities.", "An error occurred while saving the entities to the database.");
+        }
+
+        public async Task<Result<TDto>> UpdateAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+            bool saveResult = await _context.SaveChangesAsync() > 0;
+            return saveResult
+                ? Result<TDto>.Success(_mapper.MapToDto(entity))
+                : Result<TDto>.InternalFailure("Failed to update entity.", "An error occurred while saving the entity to the database.");
+        }
+
+        public async Task<Result<IEnumerable<TDto>>> UpdateRangeAsync(IEnumerable<TEntity> entity)
+        {
+            _context.Set<TEntity>().UpdateRange(entity);
+            bool saveResult = await _context.SaveChangesAsync() > 0;
+            return saveResult
+                ? Result<IEnumerable<TDto>>.Success(_mapper.MapToDto(entity))
+                : Result<IEnumerable<TDto>>.InternalFailure("Failed to update entities.", "An error occurred while saving the entities to the database.");
+        }
+
+        public async Task<bool> DeleteAsync(ISpecification<TEntity> specification)
+        {
+            TDto? result = await
+                FindFirstOrDefaultAsync(specification);
+
+            if (result is null)
+            {
+                return false;
+            }
+
+            _context.Set<TEntity>().Remove(_mapper.MapToEntity(result)!);
+
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<bool> DeleteRangeAsync(ISpecification<TEntity> specification)
+        {
+            var result = await
+                FindAsync(specification);
+
+            if (!result.Any())
+            {
+                return false;
+            }
+
+            _context.Set<TEntity>().RemoveRange(_mapper.MapToEntity(result)!);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<int> CountAsync()
+        {
+            return await _context.Set<TEntity>().CountAsync();
+        }
+
+        public async Task<bool> ExistsAsync(ISpecification<TEntity> specification)
+        {
+            return await CountAsync(specification) > 0;
+        }
     }
 
     public interface IMapper<TEntity, TDto>
         where TEntity : class
         where TDto : class
     {
-        TDto MapTo(TEntity entity);
+        TDto MapToDto(TEntity entity);
 
-        IEnumerable<TDto> MapTo(IEnumerable<TEntity> entities);
+        IEnumerable<TDto> MapToDto(IEnumerable<TEntity> entities);
 
-        TEntity MapTo(TDto dto);
+        TEntity MapToEntity(TDto dto);
 
-        IEnumerable<TEntity> MapTo(IEnumerable<TDto> dtos);
+        IEnumerable<TEntity> MapToEntity(IEnumerable<TDto> dtos);
     }
 }
