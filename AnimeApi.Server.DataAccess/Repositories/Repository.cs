e25706e4651c9f16b6cba2b7e85e8458505d694e
Jacopo <121625293+jklzz02@users.noreply.github.com>
2025-10-25 16:43:51 +0200

    Error handling for repository base class

--- AnimeApi.Server.DataAccess/Repositories/Repository.cs
+++ AnimeApi.Server.DataAccess/Repositories/Repository.cs
@@ -11,161 +11,197 @@ namespace AnimeApi.Server.DataAccess.Repositories
         where TEntity : class
         where TDto : class
     {
-        protected AnimeDbContext Context {  get; }
-        protected readonly IMapper<TEntity, TDto> _mapper;
+        protected AnimeDbContext Context { get; }
+        protected IMapper<TEntity, TDto> Mapper { get; }
 
         public Repository(AnimeDbContext context, IMapper<TEntity, TDto> mapper)
         {
             Context = context;
-            _mapper = mapper;
+            Mapper = mapper;
         }
 
+        /// <inheritdoc />
         public async Task<int> CountAsync(IQuerySpec<TEntity> specification)
         {
-            return await
-                specification
-                    .Apply(Context.Set<TEntity>())
-                    .CountAsync();
+            return await specification
+                .Apply(Context.Set<TEntity>())
+                .CountAsync();
         }
 
-
+        /// <inheritdoc />
         public async Task<IEnumerable<TDto>> FindAsync(IQuerySpec<TEntity> specification)
         {
-            List<TEntity> result = await
-                specification
-                    .Apply(Context.Set<TEntity>())
-                    .ToListAsync();
+            var entities = await specification
+                .Apply(Context.Set<TEntity>())
+                .ToListAsync();
 
-            return _mapper.MapToDto(result);
+            return Mapper.MapToDto(entities);
         }
 
+        /// <inheritdoc />
         public async Task<TDto?> FindFirstOrDefaultAsync(IQuerySpec<TEntity> specification)
         {
-            TEntity? result = await
-                specification
-                    .Apply(Context.Set<TEntity>())
-                    .FirstOrDefaultAsync();
+            var entity = await specification
+                .Apply(Context.Set<TEntity>())
+                .FirstOrDefaultAsync();
 
-            return result is null
-                ? null
-                : _mapper.MapToDto(result);
+            return entity is null ? null : Mapper.MapToDto(entity);
         }
-        
 
+        /// <inheritdoc />
         public async Task<IEnumerable<TDto>> GetAllAsync()
         {
-            List<TEntity> result = await
-                Context.Set<TEntity>()
-                    .AsNoTracking()
-                    .ToListAsync();
+            var entities = await Context.Set<TEntity>()
+                .AsNoTracking()
+                .ToListAsync();
 
-            return _mapper.MapToDto(result);
+            return Mapper.MapToDto(entities);
         }
 
+        /// <inheritdoc />
         public virtual async Task<Result<TDto>> AddAsync(TDto dto)
         {
-            var entity = _mapper.MapToEntity(dto);
-
-            Detach(entity);
-            
-           var createdEntity = await 
-                Context.Set<TEntity>()
-                    .AddAsync(entity);
-
-            bool saveResult = await Context.SaveChangesAsync() > 0;
-
-            return saveResult
-                ? Result<TDto>.Success(_mapper.MapToDto(createdEntity.Entity))
-                : Result<TDto>.InternalFailure("Failed to add entity.", "An error occurred while saving the entity to the database.");
+            try
+            {
+                var entity = Mapper.MapToEntity(dto);
+                var createdEntity = await Context.Set<TEntity>().AddAsync(entity);
+                
+                await Context.SaveChangesAsync();
+                
+                return Result<TDto>.Success(Mapper.MapToDto(createdEntity.Entity));
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                return Result<TDto>.InternalFailure(
+                    "Concurrency failure while attempting to add entity.",
+                    e.Message);
+            }
+            catch (DbUpdateException e)
+            {
+                return Result<TDto>.InternalFailure(
+                    "Failed to add entity.",
+                    e.InnerException?.Message ?? e.Message);
+            }
         }
 
+        /// <inheritdoc />
         public virtual async Task<Result<IEnumerable<TDto>>> AddRangeAsync(IEnumerable<TDto> dto)
         {
-            var entity = _mapper.MapToEntity(dto);
-
-            Detach(entity);
-            
-            await Context.Set<TEntity>().AddRangeAsync(entity);
-            bool saveResult = await Context.SaveChangesAsync() > 0;
-
-            return saveResult
-                ? Result<IEnumerable<TDto>>.Success(_mapper.MapToDto(entity))
-                : Result<IEnumerable<TDto>>.InternalFailure("Failed to add entities.", "An error occurred while saving the entities to the database.");
+            try
+            {
+                var entities = Mapper.MapToEntity(dto).ToList();
+                
+                await Context.Set<TEntity>().AddRangeAsync(entities);
+                await Context.SaveChangesAsync();
+                
+                return Result<IEnumerable<TDto>>.Success(Mapper.MapToDto(entities));
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                return Result<IEnumerable<TDto>>.InternalFailure(
+                    "Concurrency failure occurred while adding entities.",
+                    e.Message);
+            }
+            catch (DbUpdateException e)
+            {
+                return Result<IEnumerable<TDto>>.InternalFailure(
+                    "An error occurred while adding entities.",
+                    e.InnerException?.Message ?? e.Message);
+            }
         }
 
+        /// <inheritdoc />
         public virtual async Task<Result<TDto>> UpdateAsync(TDto dto)
         {
-            var entity = _mapper.MapToEntity(dto);
-
-            Detach(entity);
-            
-            Context.Set<TEntity>().Update(entity);
-            bool saveResult = await Context.SaveChangesAsync() > 0;
-            return saveResult
-                ? Result<TDto>.Success(_mapper.MapToDto(entity))
-                : Result<TDto>.InternalFailure("Failed to update entity.", "An error occurred while saving the entity to the database.");
+            try
+            {
+                var entity = Mapper.MapToEntity(dto);
+                
+                Context.Set<TEntity>().Update(entity);
+                await Context.SaveChangesAsync();
+                
+                return Result<TDto>.Success(Mapper.MapToDto(entity));
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                return Result<TDto>.InternalFailure(
+                    "Concurrency failure while attempting to update entity.",
+                    e.Message);
+            }
+            catch (DbUpdateException e)
+            {
+                return Result<TDto>.InternalFailure(
+                    "Failed to update entity.",
+                    e.InnerException?.Message ?? e.Message);
+            }
         }
 
+        /// <inheritdoc />
         public virtual async Task<Result<IEnumerable<TDto>>> UpdateRangeAsync(IEnumerable<TDto> dto)
         {
-            var entity = _mapper.MapToEntity(dto);
-
-            Detach(entity);
-            
-            Context.Set<TEntity>().UpdateRange(entity);
-            bool saveResult = await Context.SaveChangesAsync() > 0;
-            return saveResult
-                ? Result<IEnumerable<TDto>>.Success(_mapper.MapToDto(entity))
-                : Result<IEnumerable<TDto>>.InternalFailure("Failed to update entities.", "An error occurred while saving the entities to the database.");
+            try
+            {
+                var entities = Mapper.MapToEntity(dto).ToList();
+                
+                Context.Set<TEntity>().UpdateRange(entities);
+                await Context.SaveChangesAsync();
+                
+                return Result<IEnumerable<TDto>>.Success(Mapper.MapToDto(entities));
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                return Result<IEnumerable<TDto>>.InternalFailure(
+                    "Concurrency failure while updating entities.",
+                    e.Message);
+            }
+            catch (DbUpdateException e)
+            {
+                return Result<IEnumerable<TDto>>.InternalFailure(
+                    "Failed to update entities.",
+                    e.InnerException?.Message ?? e.Message);
+            }
         }
 
+        /// <inheritdoc />
         public virtual async Task<bool> DeleteAsync(IQuerySpec<TEntity> specification)
         {
-            TDto? result = await
-                FindFirstOrDefaultAsync(specification);
+            var entity = await specification
+                .Apply(Context.Set<TEntity>())
+                .FirstOrDefaultAsync();
 
-            if (result is null)
-            {
+            if (entity is null)
                 return false;
-            }
 
-            Context.Set<TEntity>().Remove(_mapper.MapToEntity(result)!);
-
+            Context.Set<TEntity>().Remove(entity);
             return await Context.SaveChangesAsync() > 0;
         }
 
+        /// <inheritdoc />
         public virtual async Task<bool> DeleteRangeAsync(IQuerySpec<TEntity> specification)
         {
-            var result = await
-                FindAsync(specification);
+            var entities = await specification
+                .Apply(Context.Set<TEntity>())
+                .ToListAsync();
 
-            if (!result.Any())
-            {
+            if (!entities.Any())
                 return false;
-            }
 
-            Context.Set<TEntity>().RemoveRange(_mapper.MapToEntity(result)!);
+            Context.Set<TEntity>().RemoveRange(entities);
             return await Context.SaveChangesAsync() > 0;
         }
 
+        /// <inheritdoc />
         public async Task<int> CountAsync()
         {
             return await Context.Set<TEntity>().CountAsync();
         }
 
+        /// <inheritdoc />
         public async Task<bool> ExistsAsync(IQuerySpec<TEntity> specification)
         {
-            return await CountAsync(specification) > 0;
-        }
-
-        protected void Detach(TEntity entity)
-        {
-            Context.Entry(entity).State = EntityState.Detached;
-        }
-
-        protected void Detach(IEnumerable<TEntity> entities)
-        {
-            entities.ToList().ForEach(Detach);
+            return await specification
+                .Apply(Context.Set<TEntity>())
+                .AnyAsync();
         }
     }
 }
