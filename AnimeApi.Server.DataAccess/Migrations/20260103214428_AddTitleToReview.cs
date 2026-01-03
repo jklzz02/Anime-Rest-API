98a2d2a52a38b2ac2c9c015d9f0ca2d3abb9ffe4
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2026-01-03 22:54:31 +0100

    migration

---
+++ AnimeApi.Server.DataAccess/Migrations/20260103214428_AddTitleToReview.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimeApi.Server.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTitleToReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "review",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "",
+                collation: "utf8mb3_general_ci");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "review");
+        }
+    }
+}
