3c74a175bc789a165a5e8ec7d4c90ca3b1506cf7
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2026-01-04 20:51:38 +0100

    Json props for detailed review

--- AnimeApi.Server.Core/Objects/Dto/ReviewDetailedDto.cs
+++ AnimeApi.Server.Core/Objects/Dto/ReviewDetailedDto.cs
@@ -1,17 +1,34 @@
-using AnimeApi.Server.Core.Objects.Models;
 using AnimeApi.Server.Core.Objects.Partials;
+using Newtonsoft.Json;
 
 namespace AnimeApi.Server.Core.Objects.Dto;
 
 public record ReviewDetailedDto
 {
+    [JsonProperty("id")]
     public int Id { get; set; }
+    
+    [JsonProperty("title")]
     public string Title { get; set; } = string.Empty;
+    
+    [JsonProperty("content")]
     public string Content { get; set; } = string.Empty;
+    
+    [JsonProperty("anime_id")]
     public int AnimeId { get; set; }
+    
+    [JsonProperty("user_id")]
     public int UserId { get; set; }
+    
+    [JsonProperty("created_at")]
     public DateTime CreatedAt { get; set; }
+    
+    [JsonProperty("user")]
     public decimal Score { get; set; }
+    
+    [JsonProperty("public_user")]
     public PublicUser User { get; init; } = new();
+    
+    [JsonProperty("anime")]
     public AnimeSummary Anime { get; init; } = new();
 }
